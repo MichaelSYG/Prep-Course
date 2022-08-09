26814ec355dc151351cb274a1d2919fae1c8547e
MichaelSYG <Michaelsyanes@gmail.com>
2022-08-09 18:09:23 -0500

    test commit number three

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -96,12 +96,24 @@ function esDiezOCinco(numero) {
   // Devuelve "true" si "numero" es 10 o 5
   // De lo contrario, devuelve "false"
   // Tu código:
+  if (numero === 10 || numero === 5) {
+    return true
+  }
+  else {
+    return false
+  }
 }
 
 function estaEnRango(numero) {
   // Devuelve "true" si "numero" es menor que 50 y mayor que 20
   // De lo contrario, devuelve "false"
   // Tu código:
+  if (numero < 50 && numero > 20) {
+    return true
+  }
+  else {
+    return false
+  }
 }
 
 function esEntero(numero) {
@@ -112,6 +124,12 @@ function esEntero(numero) {
   // De lo contrario, devuelve "false"
   // Pista: Puedes resolver esto usando `Math.floor`
   // Tu código:
+  if (numero % 1 === 0) {
+    return true
+  }
+  else {
+    return false
+  }
 }
 
 function fizzBuzz(numero) {
